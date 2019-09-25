785e6f40e51e6169dc580747ee36db4082dd2186
IgorCS <igorweb08@gmail.com>
2019-09-25 14:09:04 -0300

    firebase-storage

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -1,3 +1,32 @@
+// import {
+//     // SET_POSTS,
+//     // ADD_COMMENT,
+//     // CREATING_POST,
+//     // POST_CREATED
+//     ADD_POST, ADD_COMMENT
+// } from './actionTypes'
+// // import { setMessage } from './message'
+// import axios from 'axios'
+
+// export const addPost = post => {
+//     return dispatch => {
+//         axios.post('/posts.json', { ...post })
+//             .catch(err => console.log(err))
+//             .then(res => console.log(res.data))
+//     }
+//     // return  {
+//     //     type: ADD_POST,
+//     //     payload: post
+//     // }
+// }
+
+// export const addComment = payload => {
+//     return {
+//         type: ADD_COMMENT,
+//         payload: payload
+//     }
+// }
+
 import {
     // SET_POSTS,
     // ADD_COMMENT,
@@ -9,11 +38,32 @@ import {
 import axios from 'axios'
 
 export const addPost = post => {
+
+    // return dispatch => {
+    //     axios.post('/posts.json', { ...post })
+    //         .catch(err => console.log(err))
+    //         .then(res => console.log(res.data))
+    // }
+
     return dispatch => {
-        axios.post('/posts.json', { ...post })
+       axios({
+           url: 'uploadImage',
+           baseURL: 'https://us-central1-lambe-7ef95.cloudfunctions.net/uploadImage',
+           method: 'post',
+           data: {
+               image: post.image.base64
+           }
+       })
             .catch(err => console.log(err))
-            .then(res => console.log(res.data))
+            .then(resp => {
+                post.image = resp.data.imageUrl
+                axios.post('/posts.json', { ...post })
+                    .catch(err => console.log(err))
+                    .then(res => console.log(res.data))
+            })
     }
+
+    
     // return  {
     //     type: ADD_POST,
     //     payload: post
@@ -26,3 +76,4 @@ export const addComment = payload => {
         payload: payload
     }
 }
+
